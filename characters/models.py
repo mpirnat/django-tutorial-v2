f6896f51007e1aa20c9d147450d179ac19d429b6
Mike Pirnat <mpirnat@gmail.com>
2014-12-17 15:11:13 -0500

    Added defaults to Character fields

--- characters/models.py
+++ characters/models.py
@@ -1,4 +1,11 @@
+import random
 from django.db import models
+
+
+def generate_stat():
+    """Roll 3D6 to make a stat!"""
+    return (random.randint(1, 6) + random.randint(1, 6) +
+            random.randint(1, 6))
 
 
 class Character(models.Model):
@@ -6,18 +13,18 @@ class Character(models.Model):
     name = models.CharField(max_length=200)
     background = models.TextField()
 
-    level = models.IntegerField()
-    experience_points = models.IntegerField()
+    level = models.IntegerField(default=1)
+    experience_points = models.IntegerField(default=0)
 
-    max_hit_points = models.IntegerField()
-    current_hit_points = models.IntegerField()
+    max_hit_points = models.IntegerField(default=10)
+    current_hit_points = models.IntegerField(default=10)
 
-    strength = models.IntegerField()
-    dexterity = models.IntegerField()
-    constitution = models.IntegerField()
-    intelligence = models.IntegerField()
-    wisdom = models.IntegerField()
-    charisma = models.IntegerField()
+    strength = models.IntegerField(default=generate_stat)
+    dexterity = models.IntegerField(default=generate_stat)
+    constitution = models.IntegerField(default=generate_stat)
+    intelligence = models.IntegerField(default=generate_stat)
+    wisdom = models.IntegerField(default=generate_stat)
+    charisma = models.IntegerField(default=generate_stat)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
