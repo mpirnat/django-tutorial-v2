d8887cc32b52d66804a6800e0a45c4f7f837f5c0
Mike Pirnat <mpirnat@gmail.com>
2014-12-17 15:38:45 -0500

    Abstracted dice rolling for later reuse

--- characters/models.py
+++ characters/models.py
@@ -4,8 +4,32 @@ from django.db import models
 
 def generate_stat():
     """Roll 3D6 to make a stat!"""
-    return (random.randint(1, 6) + random.randint(1, 6) +
-            random.randint(1, 6))
+    return roll_dice(3, 6)
+
+
+def roll_dice(times, sides, modifier=0):
+    """
+    Simulate a dice roll of XdY + Z.
+
+    "Rolls" a die of Y sides X times, gets the sum, and adjusts it by an
+    optional modifier.
+
+    Example usage:
+
+       # Stats: 3d6
+       >>> roll_dice(3, 6)
+
+       # Saving throw: 1d20
+       >>> roll_dice(1, 20)
+
+       # Damage (longsword +1): 1d8 + 1
+       >>> roll_dice(1, 8, plus=1)
+
+       # Damage (cursed longsword - 2): 1d8 - 2
+       >>> roll_dice(1, 8, plus=-2)
+    """
+    randint = random.randint
+    return sum(map(lambda x: randint(1, sides), range(times))) + modifier
 
 
 class Character(models.Model):
