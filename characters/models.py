c3c5ddce268a5209fe68fbfdca210160e935186f
Mike Pirnat <mpirnat@gmail.com>
2014-12-17 11:24:56 -0500

    Created basic Character model

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+
+    name = models.CharField(max_length=200)
+    background = models.TextField()
+
+    level = models.IntegerField()
+    experience_points = models.IntegerField()
+
+    max_hit_points = models.IntegerField()
+    current_hit_points = models.IntegerField()
+
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
