162fdb0497711dcc111a08064f8e7ee0a9e8c33a
Mike Pirnat <mpirnat@gmail.com>
2014-12-23 14:10:13 -0500

    Registered Race and Class models with admin app

--- characters/admin.py
+++ characters/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from characters.models import Character
+from characters.models import Character, Race, Class
 
 admin.site.register(Character)
+admin.site.register(Race)
+admin.site.register(Class)
