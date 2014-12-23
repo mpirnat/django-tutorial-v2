d4de6a9ab4df9f993cbae763164fc2f673e2dab3
Mike Pirnat <mpirnat@gmail.com>
2014-12-23 14:11:34 -0500

    Registered Item model with admin app

--- characters/admin.py
+++ characters/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from characters.models import Character, Race, Class
+from characters.models import Character, Race, Class, Item
 
 admin.site.register(Character)
 admin.site.register(Race)
 admin.site.register(Class)
+admin.site.register(Item)
