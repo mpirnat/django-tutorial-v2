9018a6ae935d66c471599955d4b44aa4d5911e25
Mike Pirnat <mpirnat@gmail.com>
2014-12-29 15:17:54 -0500

    Created first view and URL wiring

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^characters/', include('characters.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
