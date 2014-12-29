ffcdb2b07db88f6a875136f09b02b977989cb2d1
Mike Pirnat <mpirnat@gmail.com>
2014-12-29 14:16:41 -0500

    Customized admin fieldsets for Characters

--- characters/admin.py
+++ characters/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from characters.models import Character, Race, Class, Item
 
-admin.site.register(Character)
+
+class CharacterAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'level', 'race', 'cclass', 'background']}),
+        ('Hit Points', {'fields': ['max_hit_points', 'current_hit_points']}),
+        ('Stats', {'fields': ['strength', 'dexterity', 'constitution',
+                              'intelligence', 'wisdom', 'charisma']}),
+        (None, {'fields': ['experience_points', 'inventory']}),
+    ]
+
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Race)
 admin.site.register(Class)
 admin.site.register(Item)
