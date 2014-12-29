ff238731c4f0a4a7249a1ccde2f9101a425a8568
Mike Pirnat <mpirnat@gmail.com>
2014-12-29 15:22:42 -0500

    Added stub view and URL for viewing a single character

--- characters/urls.py
+++ characters/urls.py
@@ -3,4 +3,5 @@ from characters import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<character_id>\d+)/$', views.view_character, name='view'),
 )
--- characters/views.py
+++ characters/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world.  You're at the Characters index.")
+
+
+def view_character(request, character_id):
+    return HttpResponse("You're looking at character %s." % character_id)
