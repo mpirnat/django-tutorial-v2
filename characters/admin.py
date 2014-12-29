485dcb18d157a47fcd67b9c786deeaac2366dd20
Mike Pirnat <mpirnat@gmail.com>
2014-12-29 14:33:50 -0500

    Made Character created and modified dates appear in admin interface

--- characters/admin.py
+++ characters/admin.py
@@ -3,13 +3,18 @@ from characters.models import Character, Race, Class, Item
 
 
 class CharacterAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('created', 'modified')
+
     fieldsets = [
         (None, {'fields': ['name', 'level', 'race', 'cclass', 'background']}),
         ('Hit Points', {'fields': ['max_hit_points', 'current_hit_points']}),
         ('Stats', {'fields': ['strength', 'dexterity', 'constitution',
                               'intelligence', 'wisdom', 'charisma']}),
         (None, {'fields': ['experience_points', 'inventory']}),
+        ('Date Information', {'fields': ['created', 'modified']}),
     ]
+
 
 admin.site.register(Character, CharacterAdmin)
 admin.site.register(Race)
