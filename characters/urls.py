9dff0d1a1bd5fae64b5b675f9451fd16a940d34a
Mike Pirnat <mpirnat@gmail.com>
2014-12-30 13:30:11 -0500

    Created generic delete view to remove characters

--- characters/urls.py
+++ characters/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', views.CharacterDetailView.as_view(), name='view'),
     url(r'^create/$', views.CharacterCreationView.as_view(), name='create'),
     url(r'^edit/(?P<pk>\d+)/$', views.CharacterUpdateView.as_view(), name='edit'),
+    url(r'^delete/(?P<pk>\d+)/$', views.CharacterDeleteView.as_view(), name='delete'),
 )
