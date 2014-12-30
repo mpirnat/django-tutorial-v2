41c2e140cfcc6f7263e7ef75ef1731ca773154d7
Mike Pirnat <mpirnat@gmail.com>
2014-12-30 13:00:06 -0500

    Show stats as readonly during character creation

--- characters/forms.py
+++ characters/forms.py
@@ -7,4 +7,12 @@ class CharacterForm(forms.ModelForm):
 
     class Meta:
         model = Character
-        fields = ['name', 'background', 'race', 'cclass', 'alignment']
+        fields = ['name', 'background', 'race', 'cclass', 'alignment',
+                'strength', 'dexterity', 'constitution', 'intelligence',
+                'wisdom', 'charisma', 'max_hit_points']
+
+    def __init__(self, *args, **kwargs):
+        super(CharacterForm, self).__init__(*args, **kwargs)
+        for field in ['strength', 'dexterity', 'constitution', 'intelligence',
+                'wisdom', 'charisma', 'max_hit_points']:
+            self.fields[field].widget.attrs['readonly'] = True
