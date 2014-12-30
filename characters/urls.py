0d1e7ba668d4b8147b89ee231239dae7f7ee024d
Mike Pirnat <mpirnat@gmail.com>
2014-12-30 12:04:47 -0500

    Refactored character creation view to use generic class based view

--- characters/urls.py
+++ characters/urls.py
@@ -4,5 +4,5 @@ from characters import views
 urlpatterns = patterns('',
     url(r'^$', views.CharacterIndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.CharacterDetailView.as_view(), name='view'),
-    url(r'^create/$', views.create_character, name='create'),
+    url(r'^create/$', views.CharacterCreationView.as_view(), name='create'),
 )
