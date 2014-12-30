62e53dbaf9d8851fad9764d7f738a8e029c34737
Mike Pirnat <mpirnat@gmail.com>
2014-12-30 13:12:46 -0500

    Created generic update view for characters

--- characters/urls.py
+++ characters/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', views.CharacterIndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.CharacterDetailView.as_view(), name='view'),
     url(r'^create/$', views.CharacterCreationView.as_view(), name='create'),
+    url(r'^edit/(?P<pk>\d+)/$', views.CharacterUpdateView.as_view(), name='edit'),
 )
