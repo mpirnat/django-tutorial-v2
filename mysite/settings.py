d465d7369746d39c2517f0baed0161570beb9300
Mike Pirnat <mpirnat@gmail.com>
2014-12-31 15:44:17 -0500

    Created a basic landing page for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,7 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'mysite', 'templates'),)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mysite', 'static'),)
 STATIC_URL = '/static/'
 
 
