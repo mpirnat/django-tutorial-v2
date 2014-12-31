5a5fe3cc1487fcaa87fb3b31edb91adf7da851e0
Mike Pirnat <mpirnat@gmail.com>
2014-12-31 13:11:14 -0500

    Enabled user login and logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^characters/', include('characters.urls', namespace='characters')),
     url(r'^admin/', include(admin.site.urls)),
 )
