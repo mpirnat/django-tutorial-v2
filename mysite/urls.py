7d925e9a6402ae47379fe9dd3078e02832993abe
Mike Pirnat <mpirnat@gmail.com>
2014-12-31 15:44:17 -0500

    Created a basic landing page for the site

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'mysite.views.home', name='home'),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^characters/', include('characters.urls', namespace='characters')),
