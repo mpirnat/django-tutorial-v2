9346643bd7f37bb67c56dde45e8436d20ea0c051
Mike Pirnat <mpirnat@gmail.com>
2014-12-31 14:53:41 -0500

    Require login to create, edit, or delete characters

--- characters/urls.py
+++ characters/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from characters import views
 
 urlpatterns = patterns('',
     url(r'^$', views.CharacterIndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', views.CharacterDetailView.as_view(), name='view'),
-    url(r'^create/$', views.CharacterCreationView.as_view(), name='create'),
-    url(r'^edit/(?P<pk>\d+)/$', views.CharacterUpdateView.as_view(), name='edit'),
-    url(r'^delete/(?P<pk>\d+)/$', views.CharacterDeleteView.as_view(), name='delete'),
+    url(r'^create/$', login_required(views.CharacterCreationView.as_view()), name='create'),
+    url(r'^edit/(?P<pk>\d+)/$', login_required(views.CharacterUpdateView.as_view()), name='edit'),
+    url(r'^delete/(?P<pk>\d+)/$', login_required(views.CharacterDeleteView.as_view()), name='delete'),
 )
